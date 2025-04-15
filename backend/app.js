66742d1a4b5ca3278207547a83fc457b2b7d3395
Youcef zentici <zentyoucef@email.com>
2025-04-15 18:09:04 +0100

    Update routes: added radioticket, updated notification, deleted laboticket

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,16 @@
 import express from 'express';
 import { PORT, NODE_ENV } from './config/env.js'; // assuming you saved it here
-
+import accountRoutes from './routes/accountRoutes.js'
+import authRoutes from './routes/auth.Routes.js'
+import consultationtiketRoutes from './routes/consultationtiketRoutes.js'
+import laboprescriptionRoutes from './routes/laboprescriptionRoutes.js'
+import labotiketRoutes from './routes/labotiketRoutes.js'
+import notificationRoutes from './routes/notificationRoutes.js'
+import radioprescriptionRoutes from './routes/radioticketRoutes.js'
+import refferalRoutes from './routes/referralRoutes.js'
+import serviceRoutes from './routes/serviceRoutes.js'
+import staffRoutes from './routes/staffRoutes.js'
+import userRoutes from './routes/user.Routes.js'
 const app = express();
 
 app.listen(PORT, () => {
